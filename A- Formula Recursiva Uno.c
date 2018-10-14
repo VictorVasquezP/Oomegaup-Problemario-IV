eff405fc6d6e57ac68fe56c3e377ff0a19b8ac99
VictorVasquezP <42286571+VictorVasquezP@users.noreply.github.com>
2018-10-14 00:33:14 -0500

    Add files via upload

---
+++ A- Formula Recursiva Uno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+long factorial(int n); 
+int main(int argc, char *argv[]) {
+	int n,t;
+	scanf("%d",&n);
+	factorial(n);
+	t=factorial(n);
+	printf("%d",t);
+	return 0;
+}
+long factorial(int n){
+	if(n<=5){
+		return 1;
+	}else{
+		return factorial(n-2)*5;
+	}
+}
